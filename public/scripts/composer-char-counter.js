ac6681b2fb32bc956024de6ee8f973a5f5783417
Paul Bodner <bodner80@gmail.com>
2022-02-02 13:26:52 -0800

    created and styled tweeter index html including nav bar, header, new tweet form, tweets. also added functionality to the char Counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(() => {
+  let formField = $('#tweet-text');
+  const totalChars = ($('#char-counter').text());
+
+  formField.keydown(() => {
+    const currentCount = $('#tweet-text').val().length
+    const charsLeft = (parseInt(totalChars) - (currentCount)).toString();
+    $('#char-counter').val(charsLeft);
+    if (charsLeft <= 0){
+      $('#char-counter').css("color", "red")
+    } else {
+      $('#char-counter').css("color", "#545149");
+    }
+
+  });
+  
+});
