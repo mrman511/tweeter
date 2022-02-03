ea91466075648fdc6323a50e5fe49ef941ff8677
Paul Bodner <bodner80@gmail.com>
2022-02-03 15:39:34 -0800

    improved styling of new tweet button and tweets layout

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,35 +7,6 @@
 
 $(() => {
 
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ];
-
-
-  
-
-
   //the container to appent the tweets to
   const $tweetContainer = $('#tweet-container');
   
@@ -82,10 +53,6 @@ $(() => {
     //set up tweet foot
     const $tweetFoot = $('<div>').addClass('tweet-foot');
 
-    const $flag = $('<i>').addClass('icon', 'fa-solid', 'fa-flag');
-    const $retweet = $('<i>').addClass('icon', 'fa-solid', 'fa-retweet');
-    const $like = $('<i>').addClass('icon', 'fa-solid', 'fa-heart');
-
     const $icons = `
     <div>
        <i class="icon fa-solid fa-flag"></i>
@@ -110,7 +77,6 @@ $(() => {
 
   };
   
-  //renderTweets(data);
 
   const loadTweets = () => {
     $.ajax({
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,13 @@
 $(document).ready(() => {
-  let formField = $('#tweet-text');
-  const totalChars = ($('#char-counter').text());
+  let $formField = $('#tweet-text');
+  const $totalChars = ($('#char-counter').text());
 
-  formField.keydown(() => {
-    const currentCount = $('#tweet-text').val().length;
-    const charsLeft = (parseInt(totalChars) - (currentCount)).toString();
-    $('#char-counter').val(charsLeft);
-    if (charsLeft <= 0) {
+  $formField.keydown(() => {
+    const $currentCount = ($('#tweet-text').val().length) + 1;
+    const $charsLeft = (parseInt($totalChars) - ($currentCount)).toString();
+    $('#char-counter').val($charsLeft);
+
+    if ($charsLeft <= 0) {
       $('#char-counter').css("color", "red");
     } else {
       $('#char-counter').css("color", "#545149");
